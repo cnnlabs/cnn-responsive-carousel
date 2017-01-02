6b7d8d07758880e040115bb9d00bff4f19c64c48
Ryan Poplin <ryan.poplin@turner.com>
2017-01-02 17:21:55 -0500

    clean up slider code
    I cleaned up this code base a bit and am finishing up some slider logic
    with jquery.
    
    Reviewed-By: Ryan Poplin <ryan.poplin@turner.com>
    Fix: feature/webpack-jquery-init

--- src/js/main.js
+++ src/js/main.js
@@ -2,113 +2,34 @@
 
 require('../css/style.scss');
 
+// TODO: considering some may not want to include the full jquery library in code or via cache; we should require in only the dir's from jquery needed to run this carousel...
 const $ = require('jquery');
+
+// TODO: add a config. object so options are a thing...
 
 $(($) => {
 
     const imgLength = $('.core-img').length;
-    // NOTE: will be dynamic based on responsive size
+
+    // TODO: make this dynamic based on responsive size
     let imgSlide = 800;
 
     function slide(calculation) {
-        for (let i = 0; i < imgLength; i++) {
-            let slide = $('.core-img')[i];
+        for (let j = 0; j < imgLength; j++) {
+            let slide = $('.core-img')[j];
             $(slide).animate({
                 left: `${calculation}${imgSlide}`
             }, 100, () => {
-                console.log($('.core-img').eq(i));
-                $('.core-img').eq(i);
+                $('.core-img').css('margin', '');
             });
         }
     }
 
-    // for clarity, get this to work dynamically...
     $('.left-nav-btn').click(() => {
-        slide('-=')
+        slide('-=');
     });
     $('.right-nav-btn').click(() => {
         slide('+=');
     });
 
-    // $('.core-img').css({'margin-left', '-200px'});
-    // const coreImageLength = $('.core-img').length;
-    // for (let i = 0; i < coreImageLength; i++) {
-        // for (let j = 0; j < coreImageLength; j++) {
-        //     $('.core-img')[j].animate({
-        //         left: '800px'
-        //     }, 100, () => {
-        //         console.log('asdfjkl;');
-        //     });
-        // }
-    // }
-    // $('.core-img').animate({
-    //     left: -400
-    // }, 100, () => {
-    //     $('.core-img').get(1).css('left', '');
-    // });
-    // const coreImageCount = $('.core-img').length,
-    //     thumbnailImageCount = $('.thumbnail-img').length,
-    //     singleCoreImageWidth = $('.core-img').width(),
-    //     singleThumbnailImageWidth = $('.thumbnail-img').width(),
-    //     coreImageElements = $('.core-img'),
-    //     thumbnailImageElements = $('.thumbnail-img');
-    //     // imgSliderWidth = coreImageCount * singleCoreImageWidth,
-    //     // imgThumbnailSliderWidth = thumbnailImageCount * singleThumbnailImageWidth;
-    // // $('#img-slider').css({width: imgSliderWidth});
-    // // $('#img-thumbnail-slider').css({width: imgThumbnailSliderWidth});
-    // // function moveLeft () {
-    // // }
-    // function moveRight () {
-    //     console.log(coreImageElements);
-    //     // coreImageElements.forEach((el) => {
-    //     //     el.animate({
-    //     //         left: -singleCoreImageWidth
-    //     //     }, 1000, () => {
-    //     //         console.log('...');
-    //     //     });
-    //     // });
-    // }
-    // setTimeout(() => {
-    //     moveRight();
-    // }, 1000);
 });
-
-// NOTE: jQuery for the slider
-// $(document).ready(($) => {
-//     const slideCount = $('#slider ul li').length,
-//         slideWidth = $('#slider ul li').width(),
-//         slideHeight = $('#slider ul li').height(),
-//         sliderUlWidth = slideCount * slideWidth;
-//     $('#slider ul li').css({
-//         width: window.innerWidth,
-//         height: window.innerHeight
-//     });
-//     $('#slider').css({ width: slideWidth, height: slideHeight });
-//     $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-//     $('#slider ul li:last-child').prependTo('#slider ul');
-//     function moveLeft() {
-//         $('#slider ul').animate({
-//             left: + slideWidth
-//         }, 100, () => {
-//             $('#slider ul li:last-child').prependTo('#slider ul');
-//             $('#slider ul').css('left', '');
-//         });
-//     };
-//     function moveRight() {
-//         $('#slider ul').animate({
-//             left: - slideWidth
-//         }, 100, () => {
-//             $('#slider ul li:first-child').appendTo('#slider ul');
-//             $('#slider ul').css('left', '');
-//         });
-//     };
-//     // NOTE: jQuery effect notes: make previous viewed image that's currently sliding away get a grey transparent overlay and for the loading in image; do a 'before end' type of delay for the main image sliding area.
-//     $('a.control_prev').click(() => {
-//         moveLeft();
-//     });
-//     $('a.control_next').click(() => {
-//         moveRight();
-//     });
-// });
-// when using 'webpack' to compile these items; use this first: webpack --display-modules
-// when serving this use; webpack-dev-server --progress --colors
