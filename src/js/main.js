158390406c343ff8407027e0329476727bd7fb5c
Ryan Poplin <ryan.poplin@turner.com>
2017-01-20 16:40:35 -0500

    refactor the core setup of slider
    I wanted to start fresh with a better vetted approach to solving this
    problem and have a pro ref. I was search long and hard for.
    
    Reviewed-By: Ryan Poplin <ryan.poplin@turner.com>
    Fix: feature/webpack-jquery-init

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,6 @@
 'use strict';
-
 require('../css/style.scss');
-
-// TODO: considering some may not want to include the full jquery library in code or via cache; we should require in only the dir's from jquery needed to run this carousel...
 const $ = require('jquery');
-
-// TODO: add a config. object so options are a thing...
-
 $(($) => {
-
-    const imgLength = $('.core-img').length;
-
-    // TODO: make this dynamic based on responsive size
-    let imgSlide = 800;
-
-    function slide(calculation) {
-        for (let j = 0; j < imgLength; j++) {
-            let slide = $('.core-img')[j];
-            $(slide).animate({
-                left: `${calculation}${imgSlide}`
-            }, 100, () => {
-                $('.core-img').css('margin', '');
-            });
-        }
-    }
-
-    $('.left-nav-btn').click(() => {
-        slide('-=');
-    });
-    $('.right-nav-btn').click(() => {
-        slide('+=');
-    });
 
 });
