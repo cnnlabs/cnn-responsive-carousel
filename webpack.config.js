19579cc66526066c1e2241b5db75fe2c8c128008
Ryan Poplin <ryan.poplin@turner.com>
2017-02-10 15:23:18 -0500

    setup cnn-responsive slider
    Using Toy Story as stub data. I'll need to refactor this with Hypatia
    feed data and Fave instantiations via FAVE-SERVER.
    
    Reviewed-By: Ryan Poplin <ryan.poplin@turner.com>
    Fix: feature/webpack-jquery-init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-// NOTE: we still need to add minification and uglification to webpack but we can do that before deploying for testing on mobile...
 module.exports = {
     entry: './src/js/main.js',
     output: {
@@ -9,7 +7,6 @@ module.exports = {
     devtool: 'source-map',
     module: {
         loaders: [
-            // NOTE: process our .js files to enable ES6/7 features
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -18,7 +15,6 @@ module.exports = {
                     presets: ['es2015']
                 }
             },
-            // NOTE: process our .scss files to enable .css conversions
             {
                test: /\.scss$/,
                loader: ExtractTextPlugin.extract('css!sass')
@@ -26,9 +22,11 @@ module.exports = {
         ]
     },
     plugins: [
-        // NOTE: get our processed sass and stream it to this file
         new ExtractTextPlugin('./dist/css/style.css', {
             allChunks: true
         })
-    ]
+    ],
+    node: {
+      fs: "empty"
+    }
 };
